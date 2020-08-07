cd9b0be00dff94676e4c6151bbfee848407e39e1
Thiago Bressanin <thiago@freecode.com.br>
2020-08-06 23:59:10 -0300

    Fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'fcode-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'freeCodeApp';
-  options = { center: { lat: -15.7744219, lng: -48.077959 }, zoom: 4 };
-  source = {
+export class AppComponent implements OnInit {
+  public source = {
     background: 'url("../assets/kickflip.gif") no-repeat center',
     width: '100%'
   };
+
+  public interval: any;
+
+  public progressBar = {
+    style: {
+      width: '0%'
+    },
+  }
+
+  ngOnInit(): void {
+    this.start();
+  }
+
+  public start() {
+    let current_progress = 0;
+    let step = 0.1; // the smaller this is the slower the progress bar
+    this.interval = setInterval(() => {
+      current_progress += step;
+      let progress = Math.round(Math.atan(current_progress) / (Math.PI / 2) * 100 * 1000) / 1000
+      this.progressBar.style.width = progress + '%';
+      console.log(JSON.stringify(this.progressBar));
+      if (progress >= 100) {
+        clearInterval(this.interval);
+      } else if (progress >= 60 && progress <= 80) {
+        step = 0.2
+      }
+      else if (progress >= 80 && progress <= 95) {
+        step = 0.4
+      }
+      else if (progress <= 98) {
+        step = 0.2
+      }
+      else {
+        step = 0.1
+      }
+    }, 100);
+  }
+
 }
